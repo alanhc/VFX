064c97056001fbae845ce6de200801bdb50f11d0
Hsiang <sam880222@gmail.com>
2022-03-24 00:44:35 +0800

    Bug fixes

--- hw1-HDR/mtb.py
+++ hw1-HDR/mtb.py
@@ -1,67 +1,70 @@
 import cv2
 import glob
+from matplotlib import image
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 import sys
 
-path = "../data/" + sys.argv[1] + "/*JPG"
+path = "./data/" + sys.argv[1] + "/*JPG"
 files = glob.glob(path)
 files.sort()
-images = [cv2.resize(cv2.imread(f), (200, 300),
-                     interpolation=cv2.INTER_AREA) for f in files]
+# images = [cv2.resize(cv2.imread(f), (200, 300),
+#                      interpolation=cv2.INTER_AREA) for f in files]
+images = [cv2.imread(f) for f in files]
 shutter_speed = np.array(
-    [1/32, 1/16, 1/8., 1/4, 1/2, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024])
+    [1/32, 1/16, 1/8, 1/4, 1/2, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024])
 try:
-    os.mkdir(os.path.join("../output/", sys.argv[1]))
+    os.mkdir(os.path.join("./output/", sys.argv[1]))
 except:
     print("Directory exists.")
-os.popen("cp " + files[0] + " " + os.path.join("../output/",
+os.popen("cp " + files[0] + " " + os.path.join("./output/",
          sys.argv[1], os.path.basename(files[0])))
-
-
-def Gray(img):
-    """
-    convert image to grayscale
-    """
-    return np.array([[(54*x[2]+183*x[1]+19*x[0])/256 for x in y] for y in img], dtype="uint8")
 
 
 def ImageShrink2(img):
     """
     shrink image by 2
     """
-    return np.array([[(int(img[y][x]) + int(img[y+1][x]) + int(img[y][x+1]) + int(img[y+1][x+1]))/4 for x in range(0, img.shape[1]-1, 2)] for y in range(0, img.shape[0]-1, 2)], dtype='uint8')
+    (h, w) = img.shape[:2]
+    return cv2.resize(img, (w//2, h//2))
 
 
 def ComputeBitmaps(img):
     """
     compute median threshold bitmap and exclusion bitmap
     """
-    h, w = img.shape
-    threshold = 10
+    # m = int(np.median(img))
+    # threshold_bitmap = cv2.threshold(img, m, 255, cv2.THRESH_BINARY)[1]
+
+    # exclusion_bitmap = 255 - cv2.inRange(img, m - 2, m + 2)
+
+    threshold = 2
     median = int(np.median(img))
-    threshold_bitmap = np.zeros((h, w))
-    threshold_bitmap[img > median] = 1
-    exclusion_bitmap = np.ones((h, w))
-    exclusion_bitmap[np.where(np.abs(img - median) <= threshold)] = 0
+    threshold_bitmap = np.where(img > median, 255, 0).astype(np.uint8)
+    exclusion_bitmap = np.where(
+        np.abs(img - median) <= threshold, 0, 255).astype(np.uint8)
     return threshold_bitmap, exclusion_bitmap
+    # med = int(np.median(img))
+    # thresBitmap = np.array(
+    #     [[True if yi > med else False for yi in xi] for xi in img], dtype='bool')
+    # x, y = img.shape
+    # excluBitmap = np.full((x, y), True, dtype='bool')
+    # for i in range(x):
+    #     for j in range(y):
+    #         if abs(img[i][j] - med) < 5:
+    #             excluBitmap[i][j] = False
+
+    # return thresBitmap, excluBitmap
 
 
 def BitmapShift(bm, x, y):
     """
     shift image by x, y
     """
-    ans = bm.copy()
-    if (x < 0):
-        ans[:, :-x] = ans[:, x:]
-    elif (x > 0):
-        ans[:, x:] = ans[:, :-x]
-    if (y < 0):
-        ans[:-y, :] = ans[y:, :]
-    elif (y > 0):
-        ans[y:, :] = ans[:-y, :]
-    return ans
+    M = np.float32([[1, 0, x], [0, 1, y]])
+    shape = bm.shape
+    return cv2.warpAffine(bm, M, (shape[1], shape[0]))
 
 
 # ref: [1] - Overall algorithm
@@ -76,31 +79,32 @@ def getExpShift(img1, img2, shiftBits):
         cur_shift = [0, 0]
     tb1, eb1 = ComputeBitmaps(img1)
     tb2, eb2 = ComputeBitmaps(img2)
-    min_err = img1.shape[0] * img2.shape[1]
-    for y in range(-1, 2):
-        for x in range(-1, 2):
+    min_err = img1.shape[0] * img1.shape[1]
+    shift_ret = [0, 0]
+    for x in range(-1, 2):
+        for y in range(-1, 2):
             xs = cur_shift[0] + x
             ys = cur_shift[1] + y
             shifted_tb2 = BitmapShift(tb2, xs, ys)
             shifted_eb2 = BitmapShift(eb2, xs, ys)
-            diff_b = np.logical_xor(tb1, shifted_eb2)
+            diff_b = np.logical_xor(tb1, shifted_tb2)
             diff_b = np.logical_and(diff_b, eb1)
             diff_b = np.logical_and(diff_b, shifted_eb2)
-            err = diff_b.sum()
+            err = np.sum(diff_b)
             if (err < min_err):
                 shift_ret = [xs, ys]
                 min_err = err
     return shift_ret
 
 
-level = 1
+level = 7
 image_shifted = []
+g1 = cv2.cvtColor(images[0], cv2.COLOR_BGR2GRAY)
 for i in range(1, len(images)):
-    g1 = Gray(images[0])
-    g2 = Gray(images[i])
+    g2 = cv2.cvtColor(images[i], cv2.COLOR_BGR2GRAY)
     x, y = getExpShift(g1, g2, level)
     image_shifted.append(BitmapShift(images[i], x, y))
     # print("out/"+os.path.basename(files[i]))
     cv2.imwrite(os.path.join(
-        "../output/", sys.argv[1], os.path.basename(files[i])), image_shifted[i-1])
+        "./output/", sys.argv[1], os.path.basename(files[i])), image_shifted[i-1])
     print(x, y)
